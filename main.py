7fc75cf2e564d7d9b7887fb597f9891728b12e1d
Vlad Voytenok <vleduser@gmail.com>
2018-05-25 13:42:02 +0300

    fix ip

--- main.py
+++ main.py
@@ -55,5 +55,4 @@ def add_header(r):
 
 
 if __name__=="__main__":
-    server.run(host="localhost", port=os.environ.get('PORT', 5555))
-
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
