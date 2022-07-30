30a10c4a3eaaede6760ff2a918bae19e1298d827
vled12 <vleduser@gmail.com>
2022-07-30 12:28:38 +0300

    delete unused files

--- tkp/static/lib/mode/jinja2/jinja2.js
+++ tkp/static/lib/mode/jinja2/jinja2.js
@@ -1,5 +1,6 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
+// Modified by Voitenok Vladislav to support one-line statements
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
