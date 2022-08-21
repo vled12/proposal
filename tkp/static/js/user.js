82fa4d008f419ca42c24c2f7ada6b2c31cb3e0e0
vled12 <vleduser@gmail.com>
2022-08-21 14:22:26 +0300

    dedicate modal-windows js code to separate file

--- tkp/static/js/user.js
+++ tkp/static/js/user.js
@@ -145,8 +145,6 @@ $(document).ready(function () {
 
 
 
-//Загрузка документа для его модификации
-
 var Upload = function (file) {
     this.file = file;
 };
@@ -199,28 +197,6 @@ $("#convert-file").on("change", function (e) {
     upload.doUpload();
 });
 
-
-// Modal window
-var modal = document.getElementById("convert-modal");
-var btn = document.getElementById("open-convert-dialog");
-var span = document.getElementById("convert-modal-close");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 
 /* Deprecated
